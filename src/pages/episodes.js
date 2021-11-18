94e58d1b40cd12291882a7f732dea932312adfe8
DouglasDrozda <lowdouglas9712@gmail.com>
2021-11-18 19:33:59 -0300

    removendo import footer

--- src/pages/episodes.js
+++ src/pages/episodes.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SearchInput from "../components/SearchInput";
 import MyContext from '../context/index';
